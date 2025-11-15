ac7a7e738169bd58dd297c8274b98d8ce4d78c3a
amogus <86293484+WOODEN-CHEST@users.noreply.github.com>
2025-11-15 13:52:13 +0200

    changed memory functions to not be static inline.

--- include/engine/WRMemory.h
+++ include/engine/WRMemory.h
@@ -29,6 +29,16 @@ GenericBuffer GenericBuffer_CreateVariable(void* destination,
 
 GenericBuffer GenericBuffer_CreateConstant(void* destination, size_t bufferSize, size_t elementSize, size_t elementCount);
 
+bool GenericBuffer_EnsureCapacity(GenericBuffer* buffer, size_t requiredSize);
+
+bool GenericBuffer_Write(GenericBuffer* buffer, void* itemToWrite);
+
+bool GenericBuffer_WriteUChar(GenericBuffer* buffer, unsigned char character);
+
+bool GenericBuffer_WriteString(GenericBuffer* buffer, const unsigned char* str);
+
+void GenericBuffer_TrackWrittenItems(GenericBuffer* buffer, size_t itemCount);
+
 void* Memory_Allocate(size_t size);
 
 void* Memory_Reallocate(void* ptr, size_t size);
@@ -42,56 +52,3 @@ void Memory_Zero(void* ptr, size_t size);
 bool Memory_IsEqual(void* regionA, void* regionB, size_t size);
 
 void Memory_Copy(void* source, void* destination, size_t size);
-
-static inline bool GenericBuffer_EnsureCapacity(GenericBuffer* buffer, size_t requiredSize)
-{
-    while (buffer->_bufferSize <= requiredSize)
-    {
-        size_t OldSize = buffer->_bufferSize;
-        bool WasMemoryAllocated = buffer->_requestMoreSpaceCallback && (*buffer->_requestMoreSpaceCallback)(buffer);
-        if (!WasMemoryAllocated || (buffer->_bufferSize <= OldSize))
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-static inline bool GenericBuffer_Write(GenericBuffer* buffer, void* itemToWrite)
-{
-    GenericBuffer_EnsureCapacity(buffer, (buffer->_elementCount + 1) * buffer->_elementSize);
-
-    size_t WriteOffset = buffer->_elementCount * buffer->_elementSize;
-    void* WritePosition = (void*)((uintptr_t)buffer->_buffer + WriteOffset);
-    Memory_Copy(itemToWrite, WritePosition, buffer->_elementSize);
-    buffer->_elementCount++;
-    return true;
-}
-
-static inline bool GenericBuffer_WriteUChar(GenericBuffer* buffer, unsigned char character)
-{
-    GenericBuffer_EnsureCapacity(buffer, buffer->_elementCount + 1);
-
-    size_t WriteOffset = buffer->_elementCount;
-    unsigned char* WritePosition = (unsigned char*)((uintptr_t)buffer->_buffer + WriteOffset);
-    *WritePosition = character;
-    buffer->_elementCount++;
-    return true;
-}
-
-static inline bool GenericBuffer_WriteString(GenericBuffer* buffer, const unsigned char* str)
-{
-    for (size_t i = 0; str[i] != '\0'; i++)
-    {
-        if (!GenericBuffer_WriteUChar(buffer, str[i]))
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-static inline void GenericBuffer_TrackWrittenItems(GenericBuffer* buffer, size_t itemCount)
-{
-    buffer->_elementCount += itemCount;
-}
--- source/engine/WRMemory.c
+++ source/engine/WRMemory.c
@@ -69,3 +69,56 @@ GenericBuffer GenericBuffer_CreateConstant(void* destination, size_t bufferSize,
 {
     return GenericBuffer_CreateVariable(destination, bufferSize, elementSize, elementCount, NULL, NULL);
 }
+
+static inline bool GenericBuffer_EnsureCapacity(GenericBuffer* buffer, size_t requiredSize)
+{
+    while (buffer->_bufferSize <= requiredSize)
+    {
+        size_t OldSize = buffer->_bufferSize;
+        bool WasMemoryAllocated = buffer->_requestMoreSpaceCallback && (*buffer->_requestMoreSpaceCallback)(buffer);
+        if (!WasMemoryAllocated || (buffer->_bufferSize <= OldSize))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool GenericBuffer_Write(GenericBuffer* buffer, void* itemToWrite)
+{
+    GenericBuffer_EnsureCapacity(buffer, (buffer->_elementCount + 1) * buffer->_elementSize);
+
+    size_t WriteOffset = buffer->_elementCount * buffer->_elementSize;
+    void* WritePosition = (void*)((uintptr_t)buffer->_buffer + WriteOffset);
+    Memory_Copy(itemToWrite, WritePosition, buffer->_elementSize);
+    buffer->_elementCount++;
+    return true;
+}
+
+bool GenericBuffer_WriteUChar(GenericBuffer* buffer, unsigned char character)
+{
+    GenericBuffer_EnsureCapacity(buffer, buffer->_elementCount + 1);
+
+    size_t WriteOffset = buffer->_elementCount;
+    unsigned char* WritePosition = (unsigned char*)((uintptr_t)buffer->_buffer + WriteOffset);
+    *WritePosition = character;
+    buffer->_elementCount++;
+    return true;
+}
+
+bool GenericBuffer_WriteString(GenericBuffer* buffer, const unsigned char* str)
+{
+    for (size_t i = 0; str[i] != '\0'; i++)
+    {
+        if (!GenericBuffer_WriteUChar(buffer, str[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+static inline void GenericBuffer_TrackWrittenItems(GenericBuffer* buffer, size_t itemCount)
+{
+    buffer->_elementCount += itemCount;
+}
